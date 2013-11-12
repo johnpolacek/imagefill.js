61a30466482b5c7ef561bab0bcb1ce7fc76f4056
johnpolacek <johnpolacek@gmail.com>
2013-11-12 08:57:15 -0600

    fix imagefill capitalization errors

--- js/jquery-imagefill.js
+++ js/jquery-imagefill.js
@@ -1,19 +1,19 @@
 /**
- * imageFill.js
+ * imagefill.js
  * Author & copyright (c) 2013: John Polacek
  * johnpolacek.com
  * https://twitter.com/johnpolacek
  *
  * Dual MIT & GPL license
- * 
+ *
  * Project Page: http://johnpolacek.github.io/imagefill.js
  *
  * The jQuery plugin for making images fill their containers (and be centered)
- * 
+ *
  * EXAMPLE
  * Given this html:
  * <div class="container"><img src="myawesomeimage" /></div>
- * $('.container').imageFill(); // image stretches to fill container
+ * $('.container').imagefill(); // image stretches to fill container
  *
  * REQUIRES:
  * imagesLoaded - https://github.com/desandro/imagesloaded
